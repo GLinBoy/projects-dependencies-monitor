96ce6086ef8b86036054e43a55b42719c524a502
GLinBoy <hojjatabedie@gmail.com>
2023-10-21 13:12:53 +0200

    📦 NEW: Added Project resource

---
+++ src/main/kotlin/com/glinboy/dependencymonitor/web/rest/ProjectResource.kt
@@ -0,0 +1,9 @@
+package com.glinboy.dependencymonitor.web.rest
+
+import org.springframework.web.bind.annotation.RequestMapping
+import org.springframework.web.bind.annotation.RestController
+
+@RestController
+@RequestMapping("/api/projects")
+class ProjectResource {
+}
