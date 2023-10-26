ea447484fa548dec61b41ae3a55c12f1bcb54b4f
GLinBoy <hojjatabedie@gmail.com>
2023-10-26 20:36:41 +0200

    👌 IMPROVE: Added implementation of getProjects API of ProjectRepository

--- src/main/kotlin/com/glinboy/dependencymonitor/repository/impl/ProjectRepositoryImpl.kt
+++ src/main/kotlin/com/glinboy/dependencymonitor/repository/impl/ProjectRepositoryImpl.kt
@@ -1,9 +1,19 @@
 package com.glinboy.dependencymonitor.repository.impl
 
 import com.glinboy.dependencymonitor.repository.ProjectRepository
+import com.glinboy.dependencymonitor.service.dto.ProjectDTO
 import org.jooq.DSLContext
+import org.jooq.impl.DSL
+import org.slf4j.LoggerFactory
 import org.springframework.stereotype.Repository
+import java.time.Instant
 
 @Repository
 class ProjectRepositoryImpl(private val dsl: DSLContext) : ProjectRepository {
+	private val logger = LoggerFactory.getLogger(this::class.java)
+	override fun getProjects(): List<ProjectDTO> = dsl.select()
+		.from(DSL.table(DSL.name("PROJECT"))).fetch().map {
+			ProjectDTO(it["ID"] as Long, it["TITLE"] as String, it["CREATED_AT"] as Instant, it["UPDATED_AT"] as Instant)
+		}
+		.toList()
 }
