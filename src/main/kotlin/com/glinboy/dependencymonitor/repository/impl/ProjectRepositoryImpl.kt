d3d009fbf350c6f7fa5dc3b2ea667a4fa22bb5e9
GLinBoy <hojjatabedie@gmail.com>
2023-10-26 20:37:41 +0200

    👌 IMPROVE: Added implemention getProjectById API of ProjectRepository

--- src/main/kotlin/com/glinboy/dependencymonitor/repository/impl/ProjectRepositoryImpl.kt
+++ src/main/kotlin/com/glinboy/dependencymonitor/repository/impl/ProjectRepositoryImpl.kt
@@ -16,4 +16,11 @@ class ProjectRepositoryImpl(private val dsl: DSLContext) : ProjectRepository {
 			ProjectDTO(it["ID"] as Long, it["TITLE"] as String, it["CREATED_AT"] as Instant, it["UPDATED_AT"] as Instant)
 		}
 		.toList()
+
+	override fun getProjectById(id: Long): ProjectDTO? =
+		dsl.selectFrom("PROJECT").where(DSL.field("ID").eq(id)).fetchOptional().map {
+			ProjectDTO(it["ID"] as Long, it["TITLE"] as String, it["CREATED_AT"] as Instant, it["UPDATED_AT"] as Instant)
+		}
+			.orElseGet { null }
+
 }
