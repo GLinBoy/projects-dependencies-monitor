7927a8eabb74acb0077a89578422f737ec204e4a
GLinBoy <hojjatabedie@gmail.com>
2023-10-29 13:03:02 +0100

    👌 IMPROVE: Used class generated by JOOQ to get a project from DB

--- src/main/kotlin/com/glinboy/dependencymonitor/repository/impl/ProjectRepositoryImpl.kt
+++ src/main/kotlin/com/glinboy/dependencymonitor/repository/impl/ProjectRepositoryImpl.kt
@@ -4,10 +4,8 @@ import com.glinboy.dependencymonitor.entity.Tables
 import com.glinboy.dependencymonitor.repository.ProjectRepository
 import com.glinboy.dependencymonitor.service.dto.ProjectDTO
 import org.jooq.DSLContext
-import org.jooq.impl.DSL
 import org.slf4j.LoggerFactory
 import org.springframework.stereotype.Repository
-import java.time.Instant
 
 @Repository
 class ProjectRepositoryImpl(private val dsl: DSLContext) : ProjectRepository {
@@ -24,8 +22,13 @@ class ProjectRepositoryImpl(private val dsl: DSLContext) : ProjectRepository {
 		.toList()
 
 	override fun getProjectById(id: Long): ProjectDTO? =
-		dsl.selectFrom("PROJECT").where(DSL.field("ID").eq(id)).fetchOptional().map {
-			ProjectDTO(it["ID"] as Long, it["TITLE"] as String, it["CREATED_AT"] as Instant, it["UPDATED_AT"] as Instant)
+		dsl.selectFrom(Tables.PROJECT).where(Tables.PROJECT.ID.eq(id)).fetchOptional().map {
+			ProjectDTO(
+				it[Tables.PROJECT.ID],
+				it[Tables.PROJECT.TITLE],
+				it[Tables.PROJECT.CREATED_AT],
+				it[Tables.PROJECT.UPDATED_AT]
+			)
 		}
 			.orElseGet { null }
 
