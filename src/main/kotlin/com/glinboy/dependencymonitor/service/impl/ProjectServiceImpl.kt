e1a8ccc61764f1ca3fc832b0de71ab18660839c1
GLinBoy <hojjatabedie@gmail.com>
2023-10-29 11:51:33 +0100

    👌 IMPROVE: Implemented Project service API

--- src/main/kotlin/com/glinboy/dependencymonitor/service/impl/ProjectServiceImpl.kt
+++ src/main/kotlin/com/glinboy/dependencymonitor/service/impl/ProjectServiceImpl.kt
@@ -2,8 +2,12 @@ package com.glinboy.dependencymonitor.service.impl
 
 import com.glinboy.dependencymonitor.repository.ProjectRepository
 import com.glinboy.dependencymonitor.service.ProjectService
+import com.glinboy.dependencymonitor.service.dto.ProjectDTO
 import org.springframework.stereotype.Service
 
 @Service
-class ProjectServiceImpl(val respository: ProjectRepository) : ProjectService {
+class ProjectServiceImpl(val repository: ProjectRepository) : ProjectService {
+	override fun getProjects(): List<ProjectDTO> = repository.getProjects()
+
+	override fun getProjectById(id: Long) = repository.getProjectById(id)
 }
