0d5d7c2038e5bb0896c9bc1c1d7e50fd7a16a439
GLinBoy <hojjatabedie@gmail.com>
2023-10-29 13:03:45 +0100

    👌 IMPROVE: Returned project in REST API of the projects

--- src/main/kotlin/com/glinboy/dependencymonitor/web/rest/ProjectResource.kt
+++ src/main/kotlin/com/glinboy/dependencymonitor/web/rest/ProjectResource.kt
@@ -1,6 +1,7 @@
 package com.glinboy.dependencymonitor.web.rest
 
 import com.glinboy.dependencymonitor.service.ProjectService
+import com.glinboy.dependencymonitor.service.dto.ProjectDTO
 import org.springframework.web.bind.annotation.GetMapping
 import org.springframework.web.bind.annotation.PathVariable
 import org.springframework.web.bind.annotation.RequestMapping
@@ -14,7 +15,7 @@ class ProjectResource(private val service: ProjectService) {
 	fun getProjects() = service.getProjects()
 
 	@GetMapping("/{id}")
-	fun getProjectById(@PathVariable id: Long) {
-		service.getProjectById(id)
+	fun getProjectById(@PathVariable id: Long): ProjectDTO? {
+		return service.getProjectById(id)
 	}
 }
