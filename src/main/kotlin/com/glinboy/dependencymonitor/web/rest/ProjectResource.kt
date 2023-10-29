23cd74a40bfcfc7ed882d3c70277beec2356de2c
GLinBoy <hojjatabedie@gmail.com>
2023-10-29 11:52:30 +0100

    👌 IMPROVE: Exposed Projects REST API

--- src/main/kotlin/com/glinboy/dependencymonitor/web/rest/ProjectResource.kt
+++ src/main/kotlin/com/glinboy/dependencymonitor/web/rest/ProjectResource.kt
@@ -1,9 +1,20 @@
 package com.glinboy.dependencymonitor.web.rest
 
+import com.glinboy.dependencymonitor.service.ProjectService
+import org.springframework.web.bind.annotation.GetMapping
+import org.springframework.web.bind.annotation.PathVariable
 import org.springframework.web.bind.annotation.RequestMapping
 import org.springframework.web.bind.annotation.RestController
 
 @RestController
 @RequestMapping("/api/projects")
-class ProjectResource {
+class ProjectResource(private val service: ProjectService) {
+
+	@GetMapping
+	fun getProjects() = service.getProjects()
+
+	@GetMapping("/{id}")
+	fun getProjectById(@PathVariable id: Long) {
+		service.getProjectById(id)
+	}
 }
