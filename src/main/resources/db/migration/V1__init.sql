f5444b40c637f8a35f0651db72f639a1543a7b4d
GLinBoy <hojjatabedie@gmail.com>
2023-10-30 19:49:57 +0100

    👌 IMPROVE: Added versions table

--- src/main/resources/db/migration/V1__init.sql
+++ src/main/resources/db/migration/V1__init.sql
@@ -17,4 +17,16 @@ CREATE TABLE dependency
 	created_at TIMESTAMP WITH TIME ZONE DEFAULT CURRENT_TIMESTAMP,
 	updated_at TIMESTAMP WITH TIME ZONE
 );
+
+CREATE TABLE versions
+(
+	id            BIGINT PRIMARY KEY,
+	title         VARCHAR(128),
+-- 	created_by bigint        not null REFERENCES users (id),
+-- 	updated_by bigint        not null REFERENCES users (id),
+	dependency_id BIGINT,
+	FOREIGN KEY (dependency_id) REFERENCES dependency (id),
+	created_at    TIMESTAMP WITH TIME ZONE DEFAULT CURRENT_TIMESTAMP,
+	updated_at    TIMESTAMP WITH TIME ZONE
+);
 );
