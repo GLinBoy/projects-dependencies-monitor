d8e378af979feabcfa1e0c1b943613bf913e47a4
GLinBoy <hojjatabedie@gmail.com>
2023-10-30 19:50:38 +0100

    👌 IMPROVE: Added project dependency/version relation table

--- src/main/resources/db/migration/V1__init.sql
+++ src/main/resources/db/migration/V1__init.sql
@@ -29,4 +29,12 @@ CREATE TABLE versions
 	created_at    TIMESTAMP WITH TIME ZONE DEFAULT CURRENT_TIMESTAMP,
 	updated_at    TIMESTAMP WITH TIME ZONE
 );
+
+CREATE TABLE ProjectDependency
+(
+	project_id BIGINT,
+	version_id BIGINT,
+	PRIMARY KEY (project_id, version_id),
+	FOREIGN KEY (project_id) REFERENCES Project (id),
+	FOREIGN KEY (version_id) REFERENCES versions (id)
 );
