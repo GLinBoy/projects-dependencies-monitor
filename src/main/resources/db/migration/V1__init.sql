dc62fa24bd0ffc55d5b73b2e3a22179930603550
GLinBoy <hojjatabedie@gmail.com>
2023-10-30 19:49:32 +0100

    👌 IMPROVE: Added dependency table

--- src/main/resources/db/migration/V1__init.sql
+++ src/main/resources/db/migration/V1__init.sql
@@ -7,4 +7,14 @@ CREATE TABLE project
 	created_at TIMESTAMP WITH TIME ZONE DEFAULT CURRENT_TIMESTAMP,
 	updated_at TIMESTAMP WITH TIME ZONE
 );
+
+CREATE TABLE dependency
+(
+	id         BIGINT PRIMARY KEY,
+	title      VARCHAR(128),
+-- 	created_by bigint        not null REFERENCES users (id),
+-- 	updated_by bigint        not null REFERENCES users (id),
+	created_at TIMESTAMP WITH TIME ZONE DEFAULT CURRENT_TIMESTAMP,
+	updated_at TIMESTAMP WITH TIME ZONE
+);
 );
