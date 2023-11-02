df13d574d6b2d5f5b20cb2c933e7a6336da2c8e8
GLinBoy <hojjatabedie@gmail.com>
2023-11-02 19:48:29 +0100

    👌 IMPROVE: Enhanced naming of Versions table

--- src/main/resources/db/migration/V1__init.sql
+++ src/main/resources/db/migration/V1__init.sql
@@ -21,13 +21,13 @@ CREATE TABLE dependency
 CREATE TABLE versions
 (
 	id            BIGINT PRIMARY KEY,
-	title         VARCHAR(128),
+	version_number VARCHAR(64),
 -- 	created_by bigint        not null REFERENCES users (id),
 -- 	updated_by bigint        not null REFERENCES users (id),
 	dependency_id BIGINT,
-	FOREIGN KEY (dependency_id) REFERENCES dependency (id),
 	created_at    TIMESTAMP WITH TIME ZONE DEFAULT CURRENT_TIMESTAMP,
-	updated_at    TIMESTAMP WITH TIME ZONE
+	updated_at     TIMESTAMP WITH TIME ZONE,
+	FOREIGN KEY (dependency_id) REFERENCES dependency (id)
 );
 
 CREATE TABLE ProjectDependency
