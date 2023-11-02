bc402ec0d35ff28c945779385da9a148c9a45e58
GLinBoy <hojjatabedie@gmail.com>
2023-11-02 20:35:44 +0100

    👌 IMPROVE: Added save Project endpoint

--- src/main/kotlin/com/glinboy/dependencymonitor/web/rest/ProjectResource.kt
+++ src/main/kotlin/com/glinboy/dependencymonitor/web/rest/ProjectResource.kt
@@ -4,6 +4,7 @@ import com.glinboy.dependencymonitor.service.ProjectService
 import com.glinboy.dependencymonitor.service.dto.ProjectDTO
 import org.springframework.web.bind.annotation.GetMapping
 import org.springframework.web.bind.annotation.PathVariable
+import org.springframework.web.bind.annotation.PostMapping
 import org.springframework.web.bind.annotation.RequestMapping
 import org.springframework.web.bind.annotation.RestController
 
@@ -18,4 +19,7 @@ class ProjectResource(private val service: ProjectService) {
 	fun getProjectById(@PathVariable id: Long): ProjectDTO? {
 		return service.getProjectById(id)
 	}
+
+	@PostMapping
+	fun saveProject(projectDTO: ProjectDTO) = service.saveProject(projectDTO)
 }
