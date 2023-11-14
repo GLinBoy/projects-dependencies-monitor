05747d9b14adf1bd601a81e144b0fae3898b0479
GLinBoy <hojjatabedie@gmail.com>
2023-11-14 18:28:05 +0100

    👌 IMPROVE: Added an API to get all Dependencies

--- src/main/kotlin/com/glinboy/dependencymonitor/web/rest/DependencyResource.kt
+++ src/main/kotlin/com/glinboy/dependencymonitor/web/rest/DependencyResource.kt
@@ -1,10 +1,13 @@
 package com.glinboy.dependencymonitor.web.rest
 
 import com.glinboy.dependencymonitor.service.DependencyService
-import org.springframework.web.bind.annotation.RequestMapping
-import org.springframework.web.bind.annotation.RestController
+import com.glinboy.dependencymonitor.service.dto.DependencyDTO
+import org.springframework.web.bind.annotation.*
 
 @RestController
 @RequestMapping("/api/dependencies")
 class DependencyResource(private val service: DependencyService) {
+
+	@GetMapping
+	fun getDependencies() = service.getDependencies()
 }
