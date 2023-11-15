b510b6688bff96ba293228a547b02cf23adc2899
GLinBoy <hojjatabedie@gmail.com>
2023-11-15 18:47:29 +0100

    👌 IMPROVE: Added method API to get a dependency by id to repository

--- src/main/kotlin/com/glinboy/dependencymonitor/repository/DependencyRepository.kt
+++ src/main/kotlin/com/glinboy/dependencymonitor/repository/DependencyRepository.kt
@@ -4,4 +4,5 @@ import com.glinboy.dependencymonitor.service.dto.DependencyDTO
 
 interface DependencyRepository {
 	fun getDependencies(): List<DependencyDTO>
+	fun getDependencyById(id: Long): DependencyDTO?
 }
