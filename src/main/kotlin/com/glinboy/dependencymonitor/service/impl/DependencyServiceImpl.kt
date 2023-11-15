63fd59b80dd25a7fb7dc7fc249049a1a2ee8724a
GLinBoy <hojjatabedie@gmail.com>
2023-11-15 18:42:45 +0100

    👌 IMPROVE: Added method impl. of get all dependencies

--- src/main/kotlin/com/glinboy/dependencymonitor/service/impl/DependencyServiceImpl.kt
+++ src/main/kotlin/com/glinboy/dependencymonitor/service/impl/DependencyServiceImpl.kt
@@ -1,8 +1,11 @@
 package com.glinboy.dependencymonitor.service.impl
 
+import com.glinboy.dependencymonitor.repository.DependencyRepository
 import com.glinboy.dependencymonitor.service.DependencyService
+import com.glinboy.dependencymonitor.service.dto.DependencyDTO
 import org.springframework.stereotype.Service
 
 @Service
-class DependencyServiceImpl: DependencyService {
+class DependencyServiceImpl(private val repository: DependencyRepository) : DependencyService {
+	override fun getDependencies(): List<DependencyDTO> = repository.getDependencies()
 }
