242f47cde3d7c93bced99b388f8e023d1d9e75c0
GLinBoy <hojjatabedie@gmail.com>
2023-11-15 18:48:57 +0100

    👌 IMPROVE: Added delete dependency method API to repositoy

--- src/main/kotlin/com/glinboy/dependencymonitor/repository/DependencyRepository.kt
+++ src/main/kotlin/com/glinboy/dependencymonitor/repository/DependencyRepository.kt
@@ -7,4 +7,5 @@ interface DependencyRepository {
 	fun getDependencyById(id: Long): DependencyDTO?
 	fun saveDependency(dependencyDTO: DependencyDTO): DependencyDTO?
 	fun updateDependency(dependencyDTO: DependencyDTO): DependencyDTO?
+	fun deleteDependency(id: Long): Int
 }
