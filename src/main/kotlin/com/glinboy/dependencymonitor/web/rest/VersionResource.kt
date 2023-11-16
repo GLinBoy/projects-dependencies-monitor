94ddd6970c1517ebb2e3641afe9af3a96df0a238
GLinBoy <hojjatabedie@gmail.com>
2023-11-16 20:16:59 +0100

    👌 IMPROVE: Added a method to Version API that returns all verions

--- src/main/kotlin/com/glinboy/dependencymonitor/web/rest/VersionResource.kt
+++ src/main/kotlin/com/glinboy/dependencymonitor/web/rest/VersionResource.kt
@@ -1,10 +1,13 @@
 package com.glinboy.dependencymonitor.web.rest
 
 import com.glinboy.dependencymonitor.service.VersionService
-import org.springframework.web.bind.annotation.RequestMapping
-import org.springframework.web.bind.annotation.RestController
+import com.glinboy.dependencymonitor.service.dto.VersionDTO
+import org.springframework.web.bind.annotation.*
 
 @RestController
 @RequestMapping("/api/versions")
 class VersionResource(private val service: VersionService) {
+
+	@GetMapping
+	fun getVersions() = service.getVersions()
 }
