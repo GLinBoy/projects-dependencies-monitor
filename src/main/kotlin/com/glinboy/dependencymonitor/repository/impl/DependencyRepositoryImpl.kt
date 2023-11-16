e3efc7a28dd6fe273fcd3e5934eac55a161dd7bb
GLinBoy <hojjatabedie@gmail.com>
2023-11-16 20:07:51 +0100

    👌 IMPROVE: Added a method impl. that returns all dependencies to repository

--- src/main/kotlin/com/glinboy/dependencymonitor/repository/impl/DependencyRepositoryImpl.kt
+++ src/main/kotlin/com/glinboy/dependencymonitor/repository/impl/DependencyRepositoryImpl.kt
@@ -1,8 +1,25 @@
 package com.glinboy.dependencymonitor.repository.impl
 
+import com.glinboy.dependencymonitor.entity.Tables
 import com.glinboy.dependencymonitor.repository.DependencyRepository
+import com.glinboy.dependencymonitor.service.dto.DependencyDTO
+import org.jooq.DSLContext
+import org.slf4j.LoggerFactory
 import org.springframework.stereotype.Repository
+import java.time.Instant
 
 @Repository
-class DependencyRepositoryImpl: DependencyRepository {
+class DependencyRepositoryImpl(private val dsl: DSLContext) : DependencyRepository {
+
+	private val logger = LoggerFactory.getLogger(this::class.java)
+	override fun getDependencies(): List<DependencyDTO> = dsl.select()
+		.from(Tables.DEPENDENCY).fetch().map {
+			DependencyDTO(
+				it[Tables.DEPENDENCY.ID],
+				it[Tables.DEPENDENCY.TITLE],
+				it[Tables.DEPENDENCY.CREATED_AT],
+				it[Tables.DEPENDENCY.UPDATED_AT]
+			)
+		}
+		.toList()
 }
