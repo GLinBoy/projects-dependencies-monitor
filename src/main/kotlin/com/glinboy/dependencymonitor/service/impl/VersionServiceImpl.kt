64cccb8fcb819a7ed579c12baa0427339f3c5746
GLinBoy <hojjatabedie@gmail.com>
2023-11-17 13:36:33 +0100

    👌 IMPROVE: Added a method impl to Version service impl to get all versions

--- src/main/kotlin/com/glinboy/dependencymonitor/service/impl/VersionServiceImpl.kt
+++ src/main/kotlin/com/glinboy/dependencymonitor/service/impl/VersionServiceImpl.kt
@@ -1,8 +1,11 @@
 package com.glinboy.dependencymonitor.service.impl
 
+import com.glinboy.dependencymonitor.repository.VersionRepository
 import com.glinboy.dependencymonitor.service.VersionService
+import com.glinboy.dependencymonitor.service.dto.VersionDTO
 import org.springframework.stereotype.Service
 
 @Service
-class VersionServiceImpl: VersionService {
+class VersionServiceImpl(val repository: VersionRepository) : VersionService {
+	override fun getVersions(): List<VersionDTO> = repository.getVersions()
 }
