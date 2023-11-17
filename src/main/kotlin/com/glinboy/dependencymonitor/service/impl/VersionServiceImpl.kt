c8cf66b77755dbc8df817aea32d990c887406de2
GLinBoy <hojjatabedie@gmail.com>
2023-11-17 13:37:18 +0100

    👌 IMPROVE: Added a method impl to Version service impl to get a version by id

--- src/main/kotlin/com/glinboy/dependencymonitor/service/impl/VersionServiceImpl.kt
+++ src/main/kotlin/com/glinboy/dependencymonitor/service/impl/VersionServiceImpl.kt
@@ -8,4 +8,6 @@ import org.springframework.stereotype.Service
 @Service
 class VersionServiceImpl(val repository: VersionRepository) : VersionService {
 	override fun getVersions(): List<VersionDTO> = repository.getVersions()
+
+	override fun getVersionById(id: Long) = repository.getVersionById(id)
 }
