7bc96a75a2c9eac116564e199a8ae54d19841502
GLinBoy <hojjatabedie@gmail.com>
2023-11-18 15:21:29 +0100

    👌 IMPROVE: Added get all version method impl. to Version repository impl

--- src/main/kotlin/com/glinboy/dependencymonitor/repository/impl/VersionRepositoryImpl.kt
+++ src/main/kotlin/com/glinboy/dependencymonitor/repository/impl/VersionRepositoryImpl.kt
@@ -1,8 +1,25 @@
 package com.glinboy.dependencymonitor.repository.impl
 
+import com.glinboy.dependencymonitor.entity.Tables
 import com.glinboy.dependencymonitor.repository.VersionRepository
+import com.glinboy.dependencymonitor.service.dto.VersionDTO
+import org.jooq.DSLContext
+import org.slf4j.LoggerFactory
 import org.springframework.stereotype.Repository
+import java.time.Instant
 
 @Repository
-class VersionRepositoryImpl: VersionRepository {
+class VersionRepositoryImpl(private val dsl: DSLContext): VersionRepository {
+	private val logger = LoggerFactory.getLogger(this::class.java)
+	override fun getVersions(): List<VersionDTO> = dsl.select()
+		.from(Tables.VERSIONS).fetch().map {
+			VersionDTO(
+				it[Tables.VERSIONS.ID],
+				it[Tables.VERSIONS.VERSION_NUMBER],
+				it[Tables.VERSIONS.DEPENDENCY_ID],
+				it[Tables.VERSIONS.CREATED_AT],
+				it[Tables.VERSIONS.UPDATED_AT]
+			)
+		}
+		.toList()
 }
