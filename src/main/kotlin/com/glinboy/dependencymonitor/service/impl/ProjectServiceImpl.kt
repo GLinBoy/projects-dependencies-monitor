70bfeb44b5c5224ec8637fa1c62205b255e81371
GLinBoy <hojjatabedie@gmail.com>
2023-11-19 17:34:04 +0100

    👌 IMPROVE: Added a method impl to Project service impl that return dependencies

--- src/main/kotlin/com/glinboy/dependencymonitor/service/impl/ProjectServiceImpl.kt
+++ src/main/kotlin/com/glinboy/dependencymonitor/service/impl/ProjectServiceImpl.kt
@@ -2,6 +2,7 @@ package com.glinboy.dependencymonitor.service.impl
 
 import com.glinboy.dependencymonitor.repository.ProjectRepository
 import com.glinboy.dependencymonitor.service.ProjectService
+import com.glinboy.dependencymonitor.service.dto.DependencyDTO
 import com.glinboy.dependencymonitor.service.dto.ProjectDTO
 import org.springframework.stereotype.Service
 
@@ -10,6 +11,8 @@ class ProjectServiceImpl(val repository: ProjectRepository) : ProjectService {
 	override fun getProjects(): List<ProjectDTO> = repository.getProjects()
 
 	override fun getProjectById(id: Long) = repository.getProjectById(id)
+
+	override fun getProjectDependencies(id: Long): List<DependencyDTO> = repository.getProjectDependencies(id)
 
 	override fun saveProject(projectDTO: ProjectDTO): ProjectDTO? = repository.saveProject(projectDTO)
 
