707e912b7b776f80b3b0ba0e0065cb2868163a55
GLinBoy <hojjatabedie@gmail.com>
2023-11-19 17:46:38 +0100

    👌 IMPROVE: Added a method API to the Dependency service API that return latest version

--- src/main/kotlin/com/glinboy/dependencymonitor/service/DependencyService.kt
+++ src/main/kotlin/com/glinboy/dependencymonitor/service/DependencyService.kt
@@ -1,6 +1,7 @@
 package com.glinboy.dependencymonitor.service
 
 import com.glinboy.dependencymonitor.service.dto.DependencyDTO
+import com.glinboy.dependencymonitor.service.dto.VersionDTO
 
 interface DependencyService {
 	fun getDependencies(): List<DependencyDTO>
@@ -8,4 +9,5 @@ interface DependencyService {
 	fun saveDependency(dependencyDTO: DependencyDTO): DependencyDTO?
 	fun updateDependency(dependencyDTO: DependencyDTO): DependencyDTO?
 	fun deleteDependency(id: Long)
+	fun getDependencyLatestVersion(id: Long): VersionDTO?
 }
