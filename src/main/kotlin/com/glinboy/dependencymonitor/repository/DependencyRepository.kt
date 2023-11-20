dfadd1980f7f6366e6ed70f4d9bce6f10d08091c
GLinBoy <hojjatabedie@gmail.com>
2023-11-20 12:51:07 +0100

    👌 IMPROVE: Added a method API to Dependency reposittory API that returns versions of a dependencys

--- src/main/kotlin/com/glinboy/dependencymonitor/repository/DependencyRepository.kt
+++ src/main/kotlin/com/glinboy/dependencymonitor/repository/DependencyRepository.kt
@@ -1,6 +1,7 @@
 package com.glinboy.dependencymonitor.repository
 
 import com.glinboy.dependencymonitor.service.dto.DependencyDTO
+import com.glinboy.dependencymonitor.service.dto.VersionDTO
 
 interface DependencyRepository {
 	fun getDependencies(): List<DependencyDTO>
@@ -8,4 +9,5 @@ interface DependencyRepository {
 	fun saveDependency(dependencyDTO: DependencyDTO): DependencyDTO?
 	fun updateDependency(dependencyDTO: DependencyDTO): DependencyDTO?
 	fun deleteDependency(id: Long): Int
+	fun getDependencyLatestVersion(id: Long): VersionDTO?
 }
