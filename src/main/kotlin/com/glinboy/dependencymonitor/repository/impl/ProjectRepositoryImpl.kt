239196ec0251a16649a5a9ede3abeef59ef7dcd3
GLinBoy <hojjatabedie@gmail.com>
2023-11-22 14:01:33 +0100

    👌 IMPROVE: Moved method impl that returns dependencies to Dependency repository impl

--- src/main/kotlin/com/glinboy/dependencymonitor/repository/impl/ProjectRepositoryImpl.kt
+++ src/main/kotlin/com/glinboy/dependencymonitor/repository/impl/ProjectRepositoryImpl.kt
@@ -34,29 +34,6 @@ class ProjectRepositoryImpl(private val dsl: DSLContext) : ProjectRepository {
 		}
 			.orElseGet { null }
 
-	override fun getProjectDependencies(id: Long): List<DependencyDTO> = dsl
-		.selectDistinct(
-			Tables.DEPENDENCY.ID,
-			Tables.DEPENDENCY.TITLE,
-			Tables.DEPENDENCY.CREATED_AT,
-			Tables.DEPENDENCY.UPDATED_AT
-		)
-		.from(Tables.PROJECT)
-		.leftJoin(Tables.PROJECT_DEPENDENCY).on(Tables.PROJECT_DEPENDENCY.PROJECT_ID.eq(Tables.PROJECT.ID))
-		.leftJoin(Tables.VERSIONS).on(Tables.VERSIONS.ID.eq(Tables.PROJECT_DEPENDENCY.VERSION_ID))
-		.leftJoin(Tables.DEPENDENCY).on(Tables.DEPENDENCY.ID.eq(Tables.VERSIONS.DEPENDENCY_ID))
-		.where(Tables.PROJECT.ID.eq(id))
-		.and(Tables.DEPENDENCY.ID.isNotNull)
-		.fetchArray().map {
-			DependencyDTO(
-				it[Tables.DEPENDENCY.ID],
-				it[Tables.DEPENDENCY.TITLE],
-				it[Tables.DEPENDENCY.CREATED_AT],
-				it[Tables.DEPENDENCY.UPDATED_AT]
-			)
-		}
-		.toList()
-
 	override fun saveProject(projectDTO: ProjectDTO): ProjectDTO? =
 		dsl.insertInto(Tables.PROJECT, Tables.PROJECT.TITLE)
 			.values(projectDTO.title)
