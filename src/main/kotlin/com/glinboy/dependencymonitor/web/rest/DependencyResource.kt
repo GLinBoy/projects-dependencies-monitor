1db59dc3f8cd0a73e491d9529030c176b5c654aa
GLinBoy <hojjatabedie@gmail.com>
2023-11-22 14:06:17 +0100

    👌 IMPROVE: Called version service to get latest version

--- src/main/kotlin/com/glinboy/dependencymonitor/web/rest/DependencyResource.kt
+++ src/main/kotlin/com/glinboy/dependencymonitor/web/rest/DependencyResource.kt
@@ -1,13 +1,15 @@
 package com.glinboy.dependencymonitor.web.rest
 
 import com.glinboy.dependencymonitor.service.DependencyService
+import com.glinboy.dependencymonitor.service.VersionService
 import com.glinboy.dependencymonitor.service.dto.DependencyDTO
 import com.glinboy.dependencymonitor.service.dto.VersionDTO
 import org.springframework.web.bind.annotation.*
 
 @RestController
 @RequestMapping("/api/dependencies")
-class DependencyResource(private val service: DependencyService) {
+class DependencyResource(private val service: DependencyService,
+	private val versionService: VersionService) {
 
 	@GetMapping
 	fun getDependencies() = service.getDependencies()
@@ -16,7 +18,7 @@ class DependencyResource(private val service: DependencyService) {
 	fun getDependencyById(@PathVariable id: Long): DependencyDTO? = service.getDependencyById(id)
 
 	@GetMapping("/{id}/latest")
-	fun getDependencyLatestVersion(@PathVariable id: Long): VersionDTO? = service.getDependencyLatestVersion(id)
+	fun getDependencyLatestVersion(@PathVariable id: Long): VersionDTO? = versionService.getDependencyLatestVersion(id)
 
 	@PostMapping
 	fun saveDependency(dependencyDTO: DependencyDTO) = service.saveDependency(dependencyDTO)
