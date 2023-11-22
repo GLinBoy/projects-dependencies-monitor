4167ead85f62558a78124af0af1c6b990a35cb57
GLinBoy <hojjatabedie@gmail.com>
2023-11-22 13:59:20 +0100

    👌 IMPROVE: Moved method API that returns dependencies to dependency service API

--- src/main/kotlin/com/glinboy/dependencymonitor/service/ProjectService.kt
+++ src/main/kotlin/com/glinboy/dependencymonitor/service/ProjectService.kt
@@ -6,7 +6,6 @@ import com.glinboy.dependencymonitor.service.dto.ProjectDTO
 interface ProjectService {
 	fun getProjects(): List<ProjectDTO>
 	fun getProjectById(id: Long): ProjectDTO?
-	fun getProjectDependencies(id: Long): List<DependencyDTO>
 	fun saveProject(projectDTO: ProjectDTO): ProjectDTO?
 	fun updateProject(projectDTO: ProjectDTO): ProjectDTO?
 	fun deleteProject(id: Long)
