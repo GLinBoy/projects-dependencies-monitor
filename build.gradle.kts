e590ff4320f3d027374aa2f97ee4c95f76104e41
GLinBoy <glinboy@live.com>
2024-05-11 18:59:08 +0200

    👌 IMPROVE: Upgraded dependencies dependency-management [1.1.4 -> 1.1.5]
    #92

--- build.gradle.kts
+++ build.gradle.kts
@@ -7,7 +7,7 @@ import java.time.Instant
 
 plugins {
 	id("org.springframework.boot") version "3.2.5"
-	id("io.spring.dependency-management") version "1.1.4"
+	id("io.spring.dependency-management") version "1.1.5"
 	kotlin("jvm") version "1.9.23"
 	kotlin("plugin.spring") version "1.9.23"
 	id("org.ec4j.editorconfig") version "0.1.0"
