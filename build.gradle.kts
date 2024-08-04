2c6567ecda4f158e11ed031052321d2fd779a3b9
GLinBoy <glinboy@live.com>
2024-08-04 10:09:16 +0200

    👌 IMPROVE: Upgraded Spring Boot to 3.3.2
    resolved #98

--- build.gradle.kts
+++ build.gradle.kts
@@ -6,8 +6,8 @@ import org.jooq.meta.jaxb.Logging
 import java.time.Instant
 
 plugins {
-	id("org.springframework.boot") version "3.3.1"
-	id("io.spring.dependency-management") version "1.1.5"
+	id("org.springframework.boot") version "3.3.2"
+	id("io.spring.dependency-management") version "1.1.6"
 	kotlin("jvm") version "1.9.24"
 	kotlin("plugin.spring") version "1.9.24"
 	id("org.ec4j.editorconfig") version "0.1.0"
