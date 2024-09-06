05d191e4810bbd3d496634c579911c63de747433
GLinBoy <glinboy@live.com>
2024-09-06 20:10:46 +0200

    👌 IMPROVE: Upgrade Kotlin to 1.9.25
    resolved #101

--- build.gradle.kts
+++ build.gradle.kts
@@ -8,8 +8,8 @@ import java.time.Instant
 plugins {
 	id("org.springframework.boot") version "3.3.3"
 	id("io.spring.dependency-management") version "1.1.6"
-	kotlin("jvm") version "1.9.24"
-	kotlin("plugin.spring") version "1.9.24"
+	kotlin("jvm") version "1.9.25"
+	kotlin("plugin.spring") version "1.9.25"
 	id("org.ec4j.editorconfig") version "0.1.0"
 	id("org.flywaydb.flyway") version "10.13.0"
 	id("nu.studer.jooq") version "9.0"
