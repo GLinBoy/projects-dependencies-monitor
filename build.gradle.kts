100684d13b2e711761079d1e482a4ac72b52ee7b
GLinBoy <glinboy@live.com>
2025-05-14 19:52:20 +0200

    👌 IMPROVE: Upgrade Spring Boot to 3.4.5
    resolved #113

--- build.gradle.kts
+++ build.gradle.kts
@@ -6,7 +6,7 @@ import org.jooq.meta.jaxb.Logging
 import java.time.Instant
 
 plugins {
-	id("org.springframework.boot") version "3.4.4"
+	id("org.springframework.boot") version "3.4.5"
 	id("io.spring.dependency-management") version "1.1.7"
 	kotlin("jvm") version "1.9.25"
 	kotlin("plugin.spring") version "1.9.25"
