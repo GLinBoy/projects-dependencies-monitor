092f2f69605d322f72fe200d1ec4a0d1071639e6
GLinBoy <glinboy@live.com>
2026-09-09 17:17:19 +0200

    👌 IMPROVE: Upgrade Spring Boot to 4.1.1
    closed #136

--- build.gradle.kts
+++ build.gradle.kts
@@ -7,7 +7,7 @@ import java.time.Instant
 plugins {
 	kotlin("jvm") version "2.3.21"
 	kotlin("plugin.spring") version "2.3.21"
-	id("org.springframework.boot") version "4.1.0"
+	id("org.springframework.boot") version "4.1.1"
 	id("io.spring.dependency-management") version "1.1.7"
 	id("org.ec4j.editorconfig") version "0.1.0"
 	id("org.flywaydb.flyway") version "12.9.0"
